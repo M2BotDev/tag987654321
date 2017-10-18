4a38141ed92dc2e976ad0a62f2f1117df52d04ff
Unknown <tonyvanderhall@gmail.com>
2017-10-18 14:25:34 -0400

    yay

--- runbot.py
+++ runbot.py
@@ -52,7 +52,7 @@ async def on_guild_join(guild):
         print(e)
     for channel in guild.channels:
         try:
-            await channel.send("Thanks for inviting me! Here are some things you might want to know before getting started!/n/n**Verification**/nYou can do $setverified ROLENAME to change the role that users get when verifying. If the role doesn't exist they can't verify./n**Logs**/nGo into the channel you'd like to use for logs and do $setlogchannel to receive logs there. If you don't like the logs you can do $removelogchannel to stop the bot from sending logs there. Or you can move it to a different channel./n**Prefix**/nIncase the bots prefix overlaps with another bot you can do $prefix NEWPREFIX to change it. Keep in mind if you want to change the prefix again you have to use your new prefix for the prefix command.")
+            await channel.send("Thanks for inviting me! Here are some things you might want to know before getting started!\n\n**Verification**\nYou can do $setverified ROLENAME to change the role that users get when verifying. If the role doesn't exist they can't verify.\n**Logs**\nGo into the channel you'd like to use for logs and do $setlogchannel to receive logs there. If you don't like the logs you can do $removelogchannel to stop the bot from sending logs there. Or you can move it to a different channel.\n**Prefix**\nIncase the bots prefix overlaps with another bot you can do $prefix NEWPREFIX to change it. Keep in mind if you want to change the prefix again you have to use your new prefix for the prefix command.")
             return
         except Exception as e:
             print(e)
