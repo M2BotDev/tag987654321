c0a81d8cd78e26534a501a3f4d61fef4665d9b4f
Unknown <tonyvanderhall@gmail.com>
2017-10-20 19:29:23 -0400

    x

---
+++ Commands/Owner/reset.py
@@ -0,0 +1,77 @@
+from discord.ext import commands
+import discord
+import json
+import random
+import requests
+
+class Reset:
+    """Subtracts tickets to a discord user. ``$subtract 20 @user#1234`` ``Requires you to be the owner of the discord server.``"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.type = "Roblox"
+    @commands.command(no_pm=True, pass_contex=True)
+    async def reset(self,ctx, person=None):
+        message = ctx.message
+        if message.author != message.guild.owner:
+            print("Not owner!")
+            return
+        with open("Data/servers.json", "r") as thejsonfile:
+            data = json.load(thejsonfile)
+        if str(message.guild.id) in data:
+            try:
+                data[str(message.guild.id)]["tickets-storage"][str(person.id)]["credits"] -= amount
+            except:
+                data[str(message.guild.id)]["tickets-storage"][str(person.id)] = {
+                    ("credits") : -amount
+                }
+            if person.lower() != "all":
+                if message.mentions[0]:
+                    person = message.mentions[0]
+                else:
+                    person = message.author
+                await message.channel.send(f"Reseted ``{person}``'s balance. <:nbaghost:368751631455748096> They now have ``{data[str(message.guild.id)]['tickets-storage'][str(person.id)]['credits']}`` tickets.")
+                with open("Data/servers.json", "w") as thejsonfile2:
+                    json.dump(data, thejsonfile2)
+                with open("Data/servers.json", "r") as filejson:
+                    jsondatafile = json.load(filejson)
+                if str(message.guild.id) in jsondatafile:
+                    try:
+                        whofrom = message.author.name
+                        action = f"Reseted {person}'s balance to 0."
+                        created_at = message.created_at
+                        embed=discord.Embed(title=f"From : {whofrom}", color=0x8e370d)
+                        embed.add_field(name="ACTION", value=action, inline=False)
+                        embed.set_footer(text=created_at)
+                        logchannel = discord.utils.get(message.guild.channels, id = jsondatafile[str(message.guild.id)]["log-channel"])
+                        await logchannel.send(embed=embed)
+                    except Exception as e:
+                        return
+                else:
+            else:
+                for person in data[str(message.guild.id)]["tickets-storage"]:
+                    person["credits"] = 0
+                await message.channel.send(f"Everyone now has the balance of 0.")
+                with open("Data/servers.json", "w") as thejsonfile2:
+                    json.dump(data, thejsonfile2)
+                with open("Data/servers.json", "r") as filejson:
+                    jsondatafile = json.load(filejson)
+                if str(message.guild.id) in jsondatafile:
+                    try:
+                        whofrom = message.author.name
+                        action = f"Reseted everyone's balance to 0."
+                        created_at = message.created_at
+                        embed=discord.Embed(title=f"From : {whofrom}", color=0x8e370d)
+                        embed.add_field(name="ACTION", value=action, inline=False)
+                        embed.set_footer(text=created_at)
+                        logchannel = discord.utils.get(message.guild.channels, id = jsondatafile[str(message.guild.id)]["log-channel"])
+                        await logchannel.send(embed=embed)
+                    except Exception as e:
+                        return
+
+        else:
+            await message.channel.send("For some reason your server isn't verified. Please kick and reinvite the bot!")
+            
+
+def setup(bot):
+    p = Reset(bot)
+    bot.add_cog(p)
