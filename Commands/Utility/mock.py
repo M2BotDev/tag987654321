891c44de569216fe3dae14af6bc2b1193dabc059
Unknown <tonyvanderhall@gmail.com>
2017-10-21 14:51:32 -0400

    xd

---
+++ Commands/Utility/mock.py
@@ -0,0 +1,34 @@
+from discord.ext import commands
+import discord
+import json
+import random
+import math
+
+class Mock:
+    """Makes fun of text you enter. ``$mock im super rich``"""
+    def __init__(self, bot):
+        self.bot = bot
+        self.type = "Giveaway"
+    @commands.command(no_pm=True, pass_contex=True)
+    async def mock(self,ctx,*,text=None):
+        message = ctx.message
+        if text != None:
+            newtext = ""
+            i = 0
+            for x in text:
+                i = i + 1
+                if (i / 2) == math.floor(i/2):
+                    newtext += x.upper()
+                else:
+                    newtext += x.lower()
+            embed=discord.Embed(description=newtext, color=0x8e370d)
+            embed.set_author(name=message.author.display_name, url=message.author.avatar_url, icon_url=message.author.avatar_url)
+            embed.set_thumbnail(url="http://i0.kym-cdn.com/entries/icons/original/000/022/940/spongebobicon.jpg")
+            embed.set_footer(text=message.created_at)
+            await message.channel.send(embed=embed)
+
+
+
+def setup(bot):
+    p = Mock(bot)
+    bot.add_cog(p)
